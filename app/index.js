64147fa554186081b9244a273780098611b4e8c7
Muhammad Ubaid Raza <mubaidr@gmail.com>
2017-09-08 10:29:56 +0500

    restructure, confifure eslint

--- app/index.js
+++ app/index.js
@@ -1,16 +1,15 @@
 var express = require('express')
 var path = require('path')
-var urlParser = require('url')
 
 var app = express()
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('*', (req, res, next) => {
   if (req.originalUrl === '/favicon.ico') {
-    res.status(204).end();
+    res.status(204).end()
   } else {
     console.log('Request at: ' + new Date().toUTCString())
-    next();
+    next()
   }
 })
 
